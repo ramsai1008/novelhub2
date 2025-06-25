068aab1706d8cd17fb5b6acc5678ad6fc11469f9
ramsai1008 <ramsai0014@gmail.com>
2025-06-25 17:11:40 +0000

    Fix issues in chapterid page.tsx

--- src/app/novels/[novelId]/page.tsx
+++ src/app/novels/[novelId]/page.tsx
@@ -1,4 +1,4 @@
-import { db } from '../../../lib/firebase';
+import { db } from '../lib/firebase';
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import Link from "next/link";
 
